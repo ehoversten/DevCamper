19edd9fbf0c8452bcb4a485e418b80de84491933
ehoversten <ehoversten@gmail.com>
2019-11-07 14:04:14 -0500

    added starter routes and tested with postman

--- server.js
+++ server.js
@@ -9,6 +9,44 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+
+// *** STARTER ROUTES *** //
+// GET ALL ROUTE
+app.get('/api/v1/devcamper', function(req, res) {
+    res
+      .status(200)
+      .json({ successs: true, msg: "show all bootcamps"});
+    });
+    
+// GET SINGLE RESOURCE ROUTE
+app.get('/api/v1/devcamper/:id', function(req, res) {
+    res
+        .status(200)
+        .json({ successs: true, msg: `show bootcamp with id of ${req.params.id}`});
+});
+
+// CREATE NEW RESOURCE ROUTE
+app.post('/api/v1/devcamper', function(req, res) {
+    res
+      .status(200)
+      .json({ successs: true, msg: "new bootcamp was created"});
+});
+
+// UPDATE RESOURCE ROUTE
+app.put('/api/v1/devcamper/:id', function(req, res) {
+    res
+      .status(200)
+      .json({ successs: true, msg: `updated bootcamp with id of ${req.params.id}` });
+});
+
+// DELETE RESOURCE ROUTE
+app.delete('/api/v1/devcamper/:id', function(req, res) {
+    res
+      .status(200)
+      .json({ successs: true, msg:`deleted bootcamp with id of ${req.params.id}` });
+});
+
+
 app.listen(PORT, function() {
     console.log(`Server is in ${process.env.NODE_ENV} mode on PORT: ${PORT}`);
 }) 
