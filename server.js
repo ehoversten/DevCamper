eb229d30d118a283bd25b2516ea17ff64bcf79a4
ehoversten <ehoversten@gmail.com>
2019-11-08 15:22:38 -0500

    added error handling with middleware

--- server.js
+++ server.js
@@ -21,6 +21,25 @@ app.use(morgan('dev'));
 app.use('/api/v1/devcamper', bootcamps);
 
 
+// *** ERROR MIDDLEWARE *** //
+app.use((req, res, next) => {
+    const error = new Error("Route Not Found!");
+    error.status = 404;
+    // We call the next middleware function and pass it the error;
+    next(error);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({ 
+        success: false, 
+        msg: error.message 
+    })
+});
+
+
+// *** CONNECT SERVER *** //
+
 app.listen(PORT, function() {
     console.log(`Server is in ${process.env.NODE_ENV} mode on PORT: ${PORT}`);
 }) 
