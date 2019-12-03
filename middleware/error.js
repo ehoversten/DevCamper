6acc3e574b9cefc7a52d26d94583cc17b6964b4c
ehoversten <ehoversten@gmail.com>
2019-12-03 15:38:54 -0500

    added custom Mongoose duplicate and validation errors

--- middleware/error.js
+++ middleware/error.js
@@ -14,9 +14,21 @@ const errorHandler = function(err, req, res, next) {
         error = new ErrorResponse(message, 404);
     }
 
+    // Mongoose Duplicate Key Error
+    if(err.code === 11000) {
+        const message = 'Duplcate Field Value Entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose Validation Errors
+    if(err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
+    }
+
     res.status(error.statusCode || 500).json({
         success: true,
-        error: error.messagen || 'Server Error!'
+        error: error.message || 'Server Error!'
     });
 }
 
