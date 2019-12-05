c8ec013a516828f087ed7dddc219b4d55674c74f
ehoversten <ehoversten@gmail.com>
2019-12-05 17:28:25 -0500

    added seeder file for seeding bootcamps

---
+++ models/Course.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, "Please add a Course Title"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please add a Course Description"],
+  },
+  weeks: {
+    type: String,
+    required: [true, "Please add a total course duration(in weeks)"],
+  },
+  tuition: {
+    type: String,
+    required: [true, "Please add a total course tuition"],
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, "Please add minimum skill level"],
+    enum: ["beginner", "intermediate", "advanced"],
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now
+  },
+  bootcamp: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Bootcamp',
+      required: true
+  }
+});
+
+mondule.exports = mongoose.model('Course', CourseSchema);
